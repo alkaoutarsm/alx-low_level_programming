d113902d3f20e3f3c6a5edd18f6f403137d22e4a
alkaoutarsm <smaalikawtar43@gmail.com>
2024-06-25 01:10:16 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char letter = 'z';
+
+while (letter >= 'a')
+{
+putchar(letter);
+}
+letter--;
+}
+
+putchar('\n');
+return (0);
+}
