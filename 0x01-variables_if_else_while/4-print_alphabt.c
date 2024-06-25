4936be83843c46b03b0070edee0d394a0c9dc83a
alkaoutarsm <smaalikawtar43@gmail.com>
2024-06-25 00:45:55 -0700

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 /**
  * main - Entry point of the program
  *
@@ -8,12 +9,14 @@
  */
 int main(void)
 {
-int digit = 0;
-while (digit < 10)
-{
-putchar(digit + '0');
-digit++;
-}
-putchar('\n');
-return (0);
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
+    putchar('\n');
+    return (0);
 }
