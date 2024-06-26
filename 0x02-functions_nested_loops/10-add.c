69aa56544f5663669c31dab33502b88e2f12123a
alkaoutarsm <smaalikawtar43@gmail.com>
2024-06-26 05:15:25 -0700

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add - Entry point
+ * @a: sfd
+ * @b: afd
+ * Description: Longer description of the function)?
+ * Return: 0 if success
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
