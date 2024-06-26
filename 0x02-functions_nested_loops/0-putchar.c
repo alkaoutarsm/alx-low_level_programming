bbdd5422009b8e4f40907a77a910148643824bd7
alkaoutarsm <smaalikawtar43@gmail.com>
2024-06-26 04:34:50 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include "main.h"
 
-
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- * Return: 0n success l.
- * 0n error, -l is returned, and errno is set appropriately.
+ * main - Entry point
+ * Return: Always 0
  */
+
+int main(void)
 {
-return (write(l, &c, 1));
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+    return (0);
 }
---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: 0n success l.
+ * 0n error, -l is returned, and errno is set appropriately.
+ */
+{
+return (write(l, &c, 1));
+}
+~           
